c3ee4eb597413fe7935d5c60250e2627ff25eaf2
leomoon2016 <363943770@qq.com>
2018-11-19 19:07:09 +0800

    20181119

---
+++ src/main/java/com/imooc/o2o/dao/AreaDao.java
@@ -0,0 +1,15 @@
+package com.imooc.o2o.dao;
+
+import java.util.List;
+
+import com.imooc.o2o.enerty.Area;
+
+public interface AreaDao {
+	/**
+	 * 区域列表
+	 * 
+	 * @return
+	 */
+	List<Area> queryArea();
+
+}
---
+++ src/main/java/com/imooc/o2o/enerty/ProductImg.java
@@ -0,0 +1,62 @@
+package com.imooc.o2o.enerty;
+
+import java.util.Date;
+
+public class ProductImg {
+
+	private Long productImgId;
+	private String imgAddr;
+	private String imgDesc;
+	private Integer priority;
+	private Long productId;
+	private Date createTime;
+
+	public Long getProductImgId() {
+		return productImgId;
+	}
+
+	public void setProductImgId(Long productImgId) {
+		this.productImgId = productImgId;
+	}
+
+	public String getImgAddr() {
+		return imgAddr;
+	}
+
+	public void setImgAddr(String imgAddr) {
+		this.imgAddr = imgAddr;
+	}
+
+	public String getImgDesc() {
+		return imgDesc;
+	}
+
+	public void setImgDesc(String imgDesc) {
+		this.imgDesc = imgDesc;
+	}
+
+	public Integer getPriority() {
+		return priority;
+	}
+
+	public void setPriority(Integer priority) {
+		this.priority = priority;
+	}
+
+	public Long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+}
